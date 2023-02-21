9d0aba348262e0d0e1fd0a4f53a33a2a507f6a60
ajmathis1231 <117370334+ajmathis1231@users.noreply.github.com>
2023-02-20 18:58:16 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your first name?");
+
+string firstName = Console.ReadLine();
+
+
+Console.WriteLine("What is your last name?");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Hello {0} {1}", firstName, lastName);
